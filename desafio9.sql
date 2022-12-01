6cd8963d67f2c8119fb926a79b324842e7a01373
Lucas Victor <lucassvtf@gmail.com>
2022-12-01 15:56:15 -0300

    Requisito 9

---
+++ desafio9.sql
@@ -0,0 +1,6 @@
+SELECT
+  COUNT(*) AS 'quantidade_musicas_no_historico'
+FROM SpotifyClone.usuarios AS u
+INNER JOIN SpotifyClone.historico AS h
+ON u.usuario_id = h.usuario_id
+WHERE u.nome = 'Barbara Liskov'
